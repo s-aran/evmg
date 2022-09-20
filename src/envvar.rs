815f3ee687124c150e2635643fe2a26759536dbe
s-aran <N/A>
2022-09-20 17:04:46 +0900

    fix remove does not work on Windows, and refactoring

--- src/envvar.rs
+++ src/envvar.rs
@@ -27,173 +27,202 @@ pub mod environment_variable {
 
         pub struct Environment;
 
+        impl Environment {
+            pub fn new() -> Self {
+                Environment {}
+            }
+
+            fn string_to_u16vec(s: &String) -> Vec<u16> {
+                let mut ws: Vec<u16> = s.encode_utf16().collect();
+                // terminal character
+                ws.push(0x0000);
+
+                ws
+            }
+
+            fn string_to_u8vec(s: &String) -> Vec<u8> {
+                let u16vec = Self::string_to_u16vec(s);
+                let mut u8vec: Vec<u8> = Vec::new();
+                for e in &u16vec {
+                    u8vec.push((e & 0x00FF) as u8);
+                    u8vec.push(((e & 0xFF00) >> 8) as u8);
+                }
+
+                u8vec
+            }
+
+            fn open_registry(
+                hkey: isize,
+                subkey: &String,
+                samdesired: u32,
+            ) -> Result<HKEY, String> {
+                let mut handle_key: HKEY = 0;
+                let subkey_u16vec = Self::string_to_u16vec(subkey);
+                let lpsubkey = subkey_u16vec.as_ptr();
+
+                let r = unsafe { RegOpenKeyExW(hkey, lpsubkey, 0, samdesired, &mut handle_key) };
+                match r {
+                    ERROR_SUCCESS => Ok(handle_key),
+                    _ => Err(format!(
+                        "Cannot open user environment variable. code: {}",
+                        r
+                    )),
+                }
+            }
+
+            fn close_registry(handle: HKEY) -> Result<(), String> {
+                let r = unsafe { RegCloseKey(handle) };
+                match r {
+                    ERROR_SUCCESS => Ok(()),
+                    _ => Err(format!("Cannot close registry handle. code: {}", r)),
+                }
+            }
+
+            fn read_registry(hkey: HKEY, valuename: &String) -> Result<String, String> {
+                let value_u16vec = Self::string_to_u16vec(valuename);
+                let mut size: u32 = 0;
+
+                // get actual data size
+                let calc_size_result = unsafe {
+                    RegQueryValueExW(
+                        hkey,
+                        value_u16vec.as_ptr(),
+                        std::ptr::null_mut(),
+                        std::ptr::null_mut(),
+                        std::ptr::null_mut(),
+                        &mut size,
+                    )
+                };
+
+                if calc_size_result != ERROR_SUCCESS {
+                    return Err(format!(
+                        "Cannot read user registry for get data size. code: {}",
+                        calc_size_result
+                    ));
+                }
+
+                // get data
+                let mut data = vec![0u8; size as usize];
+                let r = unsafe {
+                    RegQueryValueExW(
+                        hkey,
+                        value_u16vec.as_ptr(),
+                        std::ptr::null_mut(),
+                        std::ptr::null_mut(),
+                        data.as_mut_ptr(),
+                        &mut size,
+                    )
+                };
+
+                match r {
+                    ERROR_SUCCESS => Ok(String::from_utf8_lossy(&data).to_string()),
+                    _ => Err(format!("Cannot read user registry, code: {}", r)),
+                }
+            }
+
+            fn write_registry(hkey: HKEY, valuename: &String, data: &String) -> Result<(), String> {
+                let value_u16vec = Self::string_to_u16vec(valuename);
+                let data_u8vec = Self::string_to_u8vec(data);
+                let r = unsafe {
+                    RegSetValueExW(
+                        hkey,
+                        value_u16vec.as_ptr(),
+                        0,
+                        REG_EXPAND_SZ,
+                        data_u8vec.as_ptr(),
+                        data_u8vec.len() as u32,
+                    )
+                };
+
+                match r {
+                    ERROR_SUCCESS => Ok(()),
+                    _ => Err(format!("Cannot write user registry. code: {}", r)),
+                }
+            }
+
+            fn delete_registry(hkey: HKEY, valuename: &String) -> Result<(), String> {
+                let value_u16vec = Self::string_to_u16vec(valuename);
+                let r = unsafe { RegDeleteValueW(hkey, value_u16vec.as_ptr()) };
+
+                match r {
+                    ERROR_SUCCESS => Ok(()),
+                    _ => Err(format!("Cannot delete user registry value. code: {}", r)),
+                }
+            }
+        }
+
         impl EnvironmentVariable for Environment {
             fn list(&self) -> Vec<String> {
                 todo!()
             }
 
             fn get(&self, name: &String) -> Result<String, String> {
-                let mut handle_key: HKEY = 0;
+                let open_result =
+                    Self::open_registry(HKEY_CURRENT_USER, &ENVIRONMENT.to_string(), KEY_READ);
 
-                if unsafe {
-                    let mut wstr: Vec<u16> = ENVIRONMENT.encode_utf16().collect();
-                    // teminal character
-                    wstr.push(0x0000);
-                    RegOpenKeyExW(
-                        HKEY_CURRENT_USER,
-                        wstr.as_ptr(),
-                        0,
-                        KEY_READ,
-                        &mut handle_key,
-                    )
-                } != 0
-                {
-                    return Err("Cannot open user environment variable.".to_string());
+                if open_result.is_err() {
+                    return Err(open_result.unwrap_err());
                 }
 
-                let mut size: u32 = 0;
-                let value_str: String;
-                unsafe {
-                    let mut wstr: Vec<u16> = name.encode_utf16().collect();
-                    // teminal character
-                    wstr.push(0x0000);
+                let handle_key: HKEY = open_result.unwrap();
 
-                    // get size
-                    {
-                        let r = RegQueryValueExW(
-                            handle_key,
-                            wstr.as_ptr(),
-                            std::ptr::null_mut(),
-                            std::ptr::null_mut(),
-                            std::ptr::null_mut(),
-                            &mut size,
-                        );
+                let read_result = Self::read_registry(handle_key, name);
 
-                        if r != ERROR_SUCCESS {
-                            return Err(format!(
-                                "Cannot open user {} registry. code: {} ({:#08X})",
-                                ENVIRONMENT, r, r
-                            ));
-                        }
-                    }
-
-                    // get value
-                    let mut value = vec![0u8; size as usize];
-                    let r = RegQueryValueExW(
-                        handle_key,
-                        wstr.as_ptr(),
-                        std::ptr::null_mut(),
-                        std::ptr::null_mut(),
-                        value.as_mut_ptr(),
-                        &mut size,
-                    );
-
-                    if r != ERROR_SUCCESS {
-                        return Err(format!(
-                            "Cannot read uer {} registry. code: {} ({:#08X}",
-                            ENVIRONMENT, r, r
-                        ));
-                    }
-
-                    value_str = String::from_utf8(value).unwrap();
+                let close_result = Self::close_registry(handle_key);
+                if close_result.is_err() {
+                    return Err(close_result.unwrap_err());
                 }
 
-                if handle_key != 0 {
-                    unsafe { RegCloseKey(handle_key) };
+                if read_result.is_err() {
+                    return Err(read_result.unwrap_err());
                 }
 
-                Ok(value_str)
+                Ok(read_result.unwrap())
             }
 
             fn set(&self, name: &String, value: &String) -> Result<(), String> {
-                let mut handle_key: HKEY = 0;
+                let open_result =
+                    Self::open_registry(HKEY_CURRENT_USER, &ENVIRONMENT.to_string(), KEY_WRITE);
 
-                if unsafe {
-                    let mut wstr: Vec<u16> = ENVIRONMENT.encode_utf16().collect();
-                    // terminal character
-                    wstr.push(0x0000);
-                    RegOpenKeyExW(
-                        HKEY_CURRENT_USER,
-                        wstr.as_ptr(),
-                        0,
-                        KEY_WRITE,
-                        &mut handle_key,
-                    )
-                } != 0
-                {
-                    return Err("Cannto open user environment variable.".to_string());
+                if open_result.is_err() {
+                    return Err(open_result.unwrap_err());
                 }
 
-                unsafe {
-                    let mut name_wstr: Vec<u16> = name.encode_utf16().collect();
-                    // termianl character
-                    name_wstr.push(0x0000);
+                let handle_key: HKEY = open_result.unwrap();
 
-                    let mut value_wstr: Vec<u16> = value.encode_utf16().collect();
-                    // terminal character
-                    value_wstr.push(0x0000);
+                let write_result = Self::write_registry(handle_key, &name, &value);
 
-                    // Vec<u16> -> Vec<u8>
-                    let mut value_wstr8: Vec<u8> = Vec::new();
-                    for e in &value_wstr {
-                        value_wstr8.push((e & 0x00FF) as u8);
-                        value_wstr8.push(((e & 0xFF00) >> 8) as u8);
-                    }
-
-                    RegSetValueExW(
-                        handle_key,
-                        name_wstr.as_ptr(),
-                        0,
-                        REG_EXPAND_SZ,
-                        value_wstr8.as_ptr(),
-                        value_wstr8.len() as u32,
-                    );
+                let close_result = Self::close_registry(handle_key);
+                if close_result.is_err() {
+                    return Err(close_result.unwrap_err());
                 }
 
-                if handle_key != 0 {
-                    unsafe { RegCloseKey(handle_key) };
+                if write_result.is_err() {
+                    return Err(write_result.unwrap_err());
                 }
 
                 Ok(())
             }
 
             fn remove(&self, name: &String) -> Result<(), String> {
-                let mut handle_key: HKEY = 0;
+                let open_result =
+                    Self::open_registry(HKEY_CURRENT_USER, &ENVIRONMENT.to_string(), KEY_WRITE);
 
-                if unsafe {
-                    let mut wstr: Vec<u16> = ENVIRONMENT.encode_utf16().collect();
-                    // terminal character
-                    wstr.push(0x0000);
-                    RegOpenKeyExW(
-                        HKEY_CURRENT_USER,
-                        wstr.as_ptr(),
-                        0,
-                        KEY_ALL_ACCESS,
-                        &mut handle_key,
-                    )
-                } != 0
-                {
-                    return Err("Cannot open user environment variable.".to_string());
+                if open_result.is_err() {
+                    return Err(open_result.unwrap_err());
                 }
 
-                println!("{}", self.get(name).unwrap());
+                let handle_key: HKEY = open_result.unwrap();
 
-                unsafe {
-                    let mut name_wstr: Vec<u16> = name.encode_utf16().collect();
-                    // termianl character
-                    name_wstr.push(0x0000);
+                let delete_result = Self::delete_registry(handle_key, name);
 
-                    let r = RegDeleteKeyW(handle_key, name_wstr.as_ptr());
-                    if r != ERROR_SUCCESS {
-                        return Err(format!(
-                            "Cannot remove user environment variable. code: {}",
-                            r
-                        ));
-                    }
+                let close_result = Self::close_registry(handle_key);
+                if close_result.is_err() {
+                    return Err(close_result.unwrap_err());
                 }
 
-                if handle_key != 0 {
-                    unsafe { RegCloseKey(handle_key) };
+                if delete_result.is_err() {
+                    return Err(delete_result.unwrap_err());
                 }
 
                 Ok(())
@@ -227,12 +256,6 @@ pub mod environment_variable {
 
             fn delete_path_by_value(&self, path: &std::path::Path) -> usize {
                 todo!()
-            }
-        }
-
-        impl Environment {
-            pub fn new() -> Self {
-                Environment {}
             }
         }
     }
