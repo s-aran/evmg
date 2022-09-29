15902690c850847e87a2ce7502a58ba4b128f5d6
s.aran <s-aran@users.noreply.github.com>
2022-09-29 23:01:00 +0900

    display environment variables like a export command

--- src/main.rs
+++ src/main.rs
@@ -12,14 +12,6 @@ use std::{
 use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 
 fn main() -> io::Result<()> {
-    println!("Hello, world!");
-
-    let mut stdout = StandardStream::stdout(ColorChoice::Always);
-    stdout.set_color(ColorSpec::new().set_fg(Some(Color::Blue)))?;
-    writeln!(&mut stdout, "test")?;
-    stdout.reset()?;
-    writeln!(&mut stdout, "")?;
-
     let vargs = args::arguments::args_to_vec();
     let settings = match args::arguments::parse(&vargs) {
         Ok(s) => s,
@@ -44,9 +36,13 @@ fn main() -> io::Result<()> {
     }
 
     if settings.export.is_some() {
-        println!("export to {}.", settings.export.unwrap());
-        config::export_envvar(Path::new(settings.export.unwrap()));
-        return Ok(());
+        match config::export_envvar(Path::new(settings.export.unwrap())) {
+            Ok(_) => return Ok(()),
+            Err(e) => {
+                eprintln!("{}", e);
+                std::process::exit(1);
+            }
+        }
     }
 
     if settings.import.is_some() {
@@ -54,62 +50,23 @@ fn main() -> io::Result<()> {
         return Ok(());
     }
 
-    if settings.no_args {
-        println!("no args!!");
-    }
-
-    println!("verbose: {}", settings.verbose);
-
     let env = envvar::environment_variable::env::Environment::new();
 
     match env.list() {
-        Ok(_) => {}
+        Ok(l) => {
+            let mut stdout = StandardStream::stdout(ColorChoice::Always);
+            for (k, v) in l.iter() {
+                stdout.set_color(ColorSpec::new().set_fg(Some(Color::Cyan)))?;
+                write!(&mut stdout, "{}", k)?;
+                stdout.reset()?;
+                write!(&mut stdout, "=")?;
+                stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
+                writeln!(&mut stdout, "{}", v)?;
+            }
+            stdout.reset()?;
+        }
         Err(e) => panic!("{}", e),
     }
-
-    for i in env.get_path().unwrap() {
-        println!("{}", i);
-    }
-
-    env.set(&"TESTTEST".to_string(), &"testtest".to_string());
-
-    env.get_list(&"PATH".to_string(), &";".to_string());
-
-    env.set_list(
-        &"TESTTESTlist".to_string(),
-        &vec!["aa".to_string(), "bb".to_string(), "cc".to_string()],
-        &";".to_string(),
-    );
-
-    let alr = env.append_list(
-        &"TESTTESTlist".to_string(),
-        &"dd".to_string(),
-        &";".to_string(),
-    );
-
-    if alr.is_err() {
-        panic!("{}", alr.unwrap_err());
-    }
-
-    env.insert_list(
-        &"TESTTESTlist".to_string(),
-        &"dd".to_string(),
-        1,
-        &";".to_string(),
-    );
-
-    env.remove_list_from(
-        &"TESTTESTlist".to_string(),
-        &"dd".to_string(),
-        &";".to_string(),
-    );
-
-    env.remove_list(&"TESTTESTlist".to_string(), 1, &";".to_string());
-
-    // let err = env.remove(&"TESTTEST".to_string());
-    // if err.is_err() {
-    //     panic!("{}", err.unwrap_err());
-    // }
 
     Ok(())
 }
