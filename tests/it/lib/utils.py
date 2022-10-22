6b566041d049a22494e7d41e1942f48a5dd80869
s.aran <s-aran@users.noreply.github.com>
2022-10-22 19:53:42 +0900

    add env option for test utility

--- tests/it/lib/utils.py
+++ tests/it/lib/utils.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import sys
-import pytest
+from typing import MutableMapping
 import subprocess
 import os
 import os.path
-import shutil
 import shlex
 import platform
-import re
-import datetime
-import stat
+from typing_extensions import LiteralString
 
 
 class Utils:
@@ -22,14 +18,21 @@ class Utils:
     MainProgram = os.path.join("..", "..", "target", "release", MainProgramName)
 
     @staticmethod
-    def execute(command_line: str) -> tuple[int, str, str]:
+    def execute(
+        command_line: str, env: dict[str, str] | MutableMapping[str, str] = os.environ
+    ) -> tuple[int, str, str]:
         enc = "shiftjis" if Utils.is_windows() else "utf-8"
         sl = shlex.split(
             command_line if Utils.is_windows() else shlex.quote(command_line),
             posix=not Utils.is_windows(),
         )
         p = subprocess.Popen(
-            sl, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding=enc
+            sl,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            encoding=enc,
+            env=env,
         )
 
         p.wait()
@@ -47,20 +50,21 @@ class Utils:
 
     @staticmethod
     def assert_exec_res(
-        command_lines: str | list[str],
+        command_lines: str | list[str | LiteralString],
         expected_r: int,
         expected_o: str,
         expected_e: str,
         show_r: bool = False,
         show_o: bool = False,
         show_e: bool = False,
+        env: dict[str, str] | MutableMapping[str, str] = os.environ,
     ):
         command_line = (
             " ".join(command_lines)
             if type(command_lines) == list
             else "{}".format(command_lines)
         )
-        r, o, e = Utils.execute(command_line)
+        r, o, e = Utils.execute(command_line, env)
 
         if show_r:
             print("======== return code ========")
